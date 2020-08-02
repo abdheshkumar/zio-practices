11bf399cfc9aa29b32d9f69391ffd0b4e86f5b3b
Abdhesh Kumar <abdhesh@callhandling.co.uk>
2020-08-02 11:28:31 +0100

    Add example

--- src/test/scala/com/abtechsoft/HowToRunZIO.scala
+++ src/test/scala/com/abtechsoft/HowToRunZIO.scala
@@ -1,9 +1,9 @@
 package com.abtechsoft
 
-import zio.ZIO
+import zio.{BootstrapRuntime, Runtime, ZIO}
 import zio.console._
-import zio.Runtime
 object MainWithZIOApp extends zio.App {
+
   val program: ZIO[Console, Nothing, Unit] = putStrLn("Hello, world")
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
     //program.fold(_ => 1, _ => 0)
@@ -11,10 +11,28 @@ object MainWithZIOApp extends zio.App {
   }
 }
 
-object Main {
+object Main01 {
   val program: ZIO[Console, Nothing, Unit] = putStrLn("Hello, world")
+
   def main(args: Array[String]): Unit = {
     Runtime.default.unsafeRun(program)
-
   }
 }
+
+object Main02 {
+  val program: ZIO[Console, Nothing, Unit] = putStrLn("Hello, world")
+  val runtime = new BootstrapRuntime {}
+  def main(args: Array[String]): Unit = {
+    runtime.unsafeRun(program)
+  }
+}
+
+/*
+ZIO is a zero-dependency library for asynchronous and concurrent programming in scala.
+ZIO is a data type
+trait ZIO[-R,+E,+A]
+A description of a computation that required an environment R and may either fail with an E or succeed with an A
+-A description, not a running computation
+-Potentially asynchronous
+-A better future
+ * */
