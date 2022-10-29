518fcd2d572596110054e8a568469cc589941a25
Abdhesh Kumar <abdheshkumar90.rkg@gmail.com>
2022-10-30 00:18:08 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
     )
     val kafka = "dev.zio" %% "zio-kafka" % "2.0.1"
     val zioInteropCats =
-      "dev.zio" %% "zio-interop-cats" % "13.0.0.1"
+      "dev.zio" %% "zio-interop-cats" % "3.3.0"
     val zioLogging = "dev.zio" %% "zio-logging" % "2.1.2"
     val loggingSlf4j = "dev.zio" %% "zio-logging-slf4j" % "2.1.2"
     val zioMagic = "io.github.kitlangton" %% "zio-magic" % "0.3.12"
