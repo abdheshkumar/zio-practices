eeb1ba8954a4baae7a9c651d99e93400adec2e97
Abdhesh Kumar <abdheshkumar90.rkg@gmail.com>
2022-10-31 16:43:35 +0000

    Arrange version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,33 +3,32 @@ import sbt._
 object Dependencies {
 
   object TestContainer {
-    def testContainer(artifact: String): ModuleID =
-      "com.dimafeng" %% artifact % "0.39.12" % Test
-
-    val postgresqlContainer = testContainer("testcontainers-scala-postgresql")
+    val postgresqlContainer =
+      "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % Test
     val all = Seq(postgresqlContainer)
   }
 
   object ZIO {
-    def zioM(artifact: String): ModuleID = "dev.zio" %% artifact % "2.0.2"
-    val zio = zioM("zio")
-    val zioStreams = zioM("zio-streams")
-    val zioMacros = zioM("zio-macros")
-    val zioTest = zioM("zio-test") % Test
-    val zioTestSbt = zioM("zio-test-sbt") % Test
-    val zioTestMagnolia=  zioM("zio-test-magnolia" )% Test
+    private val zioVersion = "2.0.2"
+    val zio = "dev.zio" %% "zio" % zioVersion
+    val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
+    val zioTest = "dev.zio" %% "zio-test" % zioVersion % Test
+    val zioTestSbt = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
+    val zioTestMagnolia = "dev.zio" %% "zio-test-magnolia" % zioVersion % Test
 
+    private val zioConfigVersion = "3.0.2"
     val config = Seq(
-      "dev.zio" %% "zio-config" % "3.0.2",
-      "dev.zio" %% "zio-config-magnolia" % "3.0.2",
-      "dev.zio" %% "zio-config-typesafe" % "3.0.2",
-      "dev.zio" %% "zio-config-refined" % "3.0.2"
+      "dev.zio" %% "zio-config" % zioConfigVersion,
+      "dev.zio" %% "zio-config-magnolia" % zioConfigVersion,
+      "dev.zio" %% "zio-config-typesafe" % zioConfigVersion,
+      "dev.zio" %% "zio-config-refined" % zioConfigVersion
     )
     val kafka = "dev.zio" %% "zio-kafka" % "2.0.1"
     val zioInteropCats =
       "dev.zio" %% "zio-interop-cats" % "3.3.0"
-    val zioLogging = "dev.zio" %% "zio-logging" % "2.1.2"
-    val loggingSlf4j = "dev.zio" %% "zio-logging-slf4j" % "2.1.2"
+    private val zioLoggingVersion = "2.1.2"
+    val zioLogging = "dev.zio" %% "zio-logging" % zioLoggingVersion
+    val loggingSlf4j = "dev.zio" %% "zio-logging-slf4j" % zioLoggingVersion
 
     object Versions {
       val jaeger = "1.8.0"
@@ -48,7 +47,6 @@ object Dependencies {
     val all = Seq(
       zio,
       zioStreams,
-      zioMacros,
       zioInteropCats,
       zioLogging,
       zioTest,
@@ -59,42 +57,37 @@ object Dependencies {
     ) ++ config ++ opentelemetry
   }
   object FlyWay {
-    def flyway(artifact: String) =
-      "org.flywaydb" % artifact % "8.4.1"
-    val flyWayCore = flyway("flyway-core")
+    val flyWayCore = "org.flywaydb" % "flyway-core" % "8.4.1"
     val driver = "org.postgresql" % "postgresql" % "42.5.0"
     val all = Seq(flyWayCore, driver)
   }
 
   object Http4s {
-    def http4s(artifact: String): ModuleID =
-      "org.http4s" %% artifact % "0.23.7"
-    val http4sServer = http4s("http4s-blaze-server")
-    val http4sDsl = http4s("http4s-dsl")
-    val http4sClient = http4s("http4s-blaze-client")
-    val http4sCirce = http4s("http4s-circe")
+    private val http4sVersion = "0.23.7"
+    "org.http4s" %% "http4s-dsl" % http4sVersion
+    val http4sServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
+    val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
+    val http4sClient = "org.http4s" %% "http4s-blaze-client" % http4sVersion
+    val http4sCirce = "org.http4s" %% "http4s-circe" % http4sVersion
     val all = Seq(http4sServer, http4sDsl, http4sClient, http4sCirce)
   }
 
   object Circe {
-    def circe(artifact: String): ModuleID =
-      "io.circe" %% artifact % "0.14.1"
-    val circeCore = circe("circe-core")
-    val circeGeneric = circe("circe-generic")
-    val circeParser = circe("circe-parser")
+    private val circeVersion = "0.14.1"
+    val circeCore = "io.circe" %% "circe-core" % circeVersion
+    val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
+    val circeParser = "io.circe" %% "circe-parser" % circeVersion
     val all = Seq(circeCore, circeGeneric, circeParser)
   }
 
   object Doobie {
-    def doobie(artifact: String): ModuleID =
-      "org.tpolecat" %% artifact % "1.0.0-RC2"
-
-    val core = doobie("doobie-core")
-    val hikari = doobie("doobie-hikari")
-    val postgres = doobie("doobie-postgres")
-    val refined = doobie("doobie-refined")
+    private val doobieVersion = "1.0.0-RC2"
+    val core = "org.tpolecat" %% "doobie-core" % doobieVersion
+    val hikari = "org.tpolecat" %% "doobie-hikari" % doobieVersion
+    val postgres = "org.tpolecat" %% "doobie-postgres" % doobieVersion
+    val refined = "org.tpolecat" %% "doobie-refined" % doobieVersion
     val quill = "io.getquill" %% "quill-doobie" % "4.6.0"
-    val doobieH2 = doobie("doobie-h2")
+    val doobieH2 = "org.tpolecat" %% "doobie-h2" % doobieVersion
     val quillJdbc = "io.getquill" %% "quill-jdbc" % "4.6.0"
     val h2 = "com.h2database" % "h2" % "2.1.214"
     val all: Seq[ModuleID] =
